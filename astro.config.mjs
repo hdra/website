bad085fcdbca511ac8d8fe0f6c5f16fc43db3001
Hendra <hendra2392@gmail.com>
2026-06-29 21:08:50 +0700

    update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://beta.hendra.dev',
+	site: 'https://hendra.dev',
 	vite: {
 		server: {
 			allowedHosts: ['.ts.net'],
